563f0d75112584da99b736ef9f94d181680389e6
Dknife <young.min.kang@gmail.com>
2017-03-21 14:47:15 +0900

    simple engine

--- SimpleEngine/SimpleEngine/main.cpp
+++ SimpleEngine/SimpleEngine/main.cpp
@@ -7,24 +7,28 @@
 
 class CMyWindow : public CKangGL {
 public:
-	CMyWindow() : CKangGL() {}
-	~CMyWindow() {};
+	CMyWindow() : CKangGL() {
+		point = addPoint();
+		addPlane(5, 5, 0.2);
+		setCamera(5, 5, 5, 0, 0, 0, 0, 1, 0);
+	}
+	~CMyWindow() {
+		removePoints();
+	};
 
 	CPoint *point;
 
 	virtual void init(void) {
-		point = addPoint();
-		addPlane(5, 5, 0.2);
-		
+
 	};
 
 	virtual void clean(void) {
-		removePoints();
+		
 	}
 	
 	virtual void doSimulation(double dt, double currentTime) { // time in seconds
-		setCamera(sin(currentTime*6.28), 5, 5, 0, 0, 0, 0, 1, 0);
-		point->setLocation(0, sin(currentTime*6.28), 0);		
+		
+		point->setLocation(cos(currentTime*6.28), 3, sin(currentTime*6.28));		
 	}
 };
 
