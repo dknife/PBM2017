7df2181a1a6835bdd836feedd9bd46a2299a3855
Dknife <young.min.kang@gmail.com>
2017-05-16 16:32:09 +0900

    colliding spheres

--- ExCode/Ex2017_06_01_CollidingSpheres/Ex2017_06_01_CollidingSpheres/main.cpp
+++ ExCode/Ex2017_06_01_CollidingSpheres/Ex2017_06_01_CollidingSpheres/main.cpp
@@ -17,10 +17,6 @@ double rand(double minV, double maxV) {
 class CMyWindow : public CKangGL {
 public:
 	sphere *sphere[NSPHERES];
-	
-
-	float density;
-	float dragCoeff;
 
 	CMyWindow() : CKangGL() {
 		for(int i=0;i<NSPHERES;i++) sphere[i] = addSphere();
@@ -38,9 +34,9 @@ public:
 		sphere[0]->setRadius(1.0);
 
 		sphere[1]->setLocation(vec3d(5, 0, 0));
-		sphere[1]->setVelocity(vec3d(-5, 0, 0));
+		sphere[1]->setVelocity(vec3d(-5000, 0, 0));
 		sphere[1]->setAcc(vec3d(0, 0, 0));
-		sphere[1]->setRadius(1.0);
+		sphere[1]->setRadius(0.2);
 		
 	};
 
@@ -49,9 +45,36 @@ public:
 	}
 
 	virtual void doSimulation(double dt, double currentTime) { // time in seconds
-		for (int i = 0; i < NSPHERES; i++) {
-			sphere[i]->simulate(dt);
-			sphere[i]->initAcc();
+		dt = 0.0001;
+		for (int iteration = 0; iteration < 10; iteration++) {
+
+			for (int i = 0; i < NSPHERES; i++) {
+				sphere[i]->simulate(dt);
+					sphere[i]->initAcc();
+			}
+
+			// collision handling
+			for (int i = 0; i < NSPHERES; i++) {
+				for (int j = i + 1; j < NSPHERES; j++) {
+					vec3d p1 = sphere[i]->getLocation();
+					vec3d p2 = sphere[j]->getLocation();
+					float distance = (p1 - p2).len();
+					float r1 = sphere[i]->getRadius();
+					float r2 = sphere[j]->getRadius();
+					float m1 = sphere[i]->mass;
+					float m2 = sphere[j]->mass;
+					vec3d v1 = sphere[i]->getVelocity();
+					vec3d v2 = sphere[j]->getVelocity();
+					vec3d v1new;
+					vec3d v2new;
+					if (distance < r1 + r2) {
+						v1new = (2 * m2*v2 + (m1 - m2)*v1) / (m1 + m2);
+						v2new = (2 * m1*v1 + (m2 - m1)*v2) / (m1 + m2);
+						sphere[i]->setVelocity(v1new);
+						sphere[j]->setVelocity(v2new);
+					}
+				}
+			}
 		}
 	}
 };
