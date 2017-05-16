7d5753d910b94257742d4b7ab2a6ed6c0acb2071
Dknife <young.min.kang@gmail.com>
2017-05-16 17:36:00 +0900

    colliding spheres

--- ExCode/Ex2017_06_01_CollidingSpheres/Ex2017_06_01_CollidingSpheres/main.cpp
+++ ExCode/Ex2017_06_01_CollidingSpheres/Ex2017_06_01_CollidingSpheres/main.cpp
@@ -21,22 +21,29 @@ public:
 	CMyWindow() : CKangGL() {
 		for(int i=0;i<NSPHERES;i++) sphere[i] = addSphere();
 		addPlane(50, 50, 5);
-		setCamera(0, 5, 10, 0, 0, 0, 0, 1, 0);		
+		setCamera(0, 15, 0, 0, 0, 0, 0, 0, 1);		
 	}
 	~CMyWindow() {
 		removeSpheres();
 	};
 
 	virtual void init(void) {
-		sphere[0]->setLocation(vec3d(-5, 0, 0));
-		sphere[0]->setVelocity(vec3d(5, 0, 0));
+		/*for (int i = 0; i < NSPHERES; i++) {
+			vec3d p(rand(-15, 15), 0, rand(-15, 15));
+			sphere[i]->setLocation(p);
+			sphere[i]->setVelocity(-p);
+			sphere[i]->setAcc(vec3d(0, 0, 0));
+			sphere[i]->setRadius(rand(0.5,1.0));
+		}*/
+		sphere[0]->setLocation(vec3d(-5,0,1.75));
+		sphere[0]->setVelocity(vec3d(5,0,0));
 		sphere[0]->setAcc(vec3d(0, 0, 0));
 		sphere[0]->setRadius(1.0);
 
 		sphere[1]->setLocation(vec3d(5, 0, 0));
-		sphere[1]->setVelocity(vec3d(-5000, 0, 0));
+		sphere[1]->setVelocity(vec3d(-5, 0, 0));
 		sphere[1]->setAcc(vec3d(0, 0, 0));
-		sphere[1]->setRadius(0.2);
+		sphere[1]->setRadius(0.8);
 		
 	};
 
@@ -45,9 +52,8 @@ public:
 	}
 
 	virtual void doSimulation(double dt, double currentTime) { // time in seconds
-		dt = 0.0001;
+		dt = 0.001;
 		for (int iteration = 0; iteration < 10; iteration++) {
-
 			for (int i = 0; i < NSPHERES; i++) {
 				sphere[i]->simulate(dt);
 					sphere[i]->initAcc();
@@ -55,23 +61,26 @@ public:
 
 			// collision handling
 			for (int i = 0; i < NSPHERES; i++) {
-				for (int j = i + 1; j < NSPHERES; j++) {
+				for (int j = i + 1; j < NSPHERES; j++) {					
 					vec3d p1 = sphere[i]->getLocation();
 					vec3d p2 = sphere[j]->getLocation();
+					vec3d N = (p1-p2).getNormalized();
 					float distance = (p1 - p2).len();
 					float r1 = sphere[i]->getRadius();
 					float r2 = sphere[j]->getRadius();
 					float m1 = sphere[i]->mass;
 					float m2 = sphere[j]->mass;
-					vec3d v1 = sphere[i]->getVelocity();
-					vec3d v2 = sphere[j]->getVelocity();
-					vec3d v1new;
-					vec3d v2new;
+					float v1 = dot(sphere[i]->getVelocity(), N);
+					float v2 = dot(sphere[j]->getVelocity(), N);
+					float v1new;
+					float v2new;
 					if (distance < r1 + r2) {
 						v1new = (2 * m2*v2 + (m1 - m2)*v1) / (m1 + m2);
 						v2new = (2 * m1*v1 + (m2 - m1)*v2) / (m1 + m2);
-						sphere[i]->setVelocity(v1new);
-						sphere[j]->setVelocity(v2new);
+						sphere[i]->setVelocity(
+							sphere[i]->getVelocity() + (v1new-v1)*N);
+						sphere[j]->setVelocity(
+							sphere[j]->getVelocity() + (v2new-v2)*N);
 					}
 				}
 			}
